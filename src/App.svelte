f847e591e375c578a1abc47095e9291776af5c76
Rudolf Hersén <rudolf.hersen@schibsted.se>
2019-10-20 22:01:12 +0200

    one arrow for each direction

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,56 @@
 <!--suppress HtmlUnknownAttribute -->
 <script>
   import locations from './locations'
+  import * as grid from "./grid"
   import Trains from "./Trains.svelte"
 
   let result = {TrainAnnouncement: []};
 
-  async function getRandomNumber() {
-    const res = await fetch(`.netlify/functions/node-fetch/direction=n`);
-    const text = await res.text();
+  function getCurrent(direction) {
+    return async () => {
+      const res = await fetch(`.netlify/functions/node-fetch?direction=${direction}`);
+      const json = await res.json();
 
-    if (res.ok) {
-      return text;
-    } else {
-      throw new Error(text);
+      if (res.ok) {
+        result = json;
+      } else {
+        throw new Error(json);
+      }
     }
-  }
-
-  async function handleClick() {
-    result = JSON.parse(await getRandomNumber())
   }
 </script>
 
-<button on:click={handleClick}>northbound</button>
+<style>
+  polygon {
+    stroke-width: 0.02px
+  }
 
-<Trains result={result} stations={locations()}></Trains>
+  polygon.clicked {
+    fill: gray
+  }
+
+  polygon.loaded {
+    fill: hsl(240, 100%, 50%)
+  }
+</style>
+
+
+<svg viewBox="-4 -6 8 12">
+  <polygon
+      points={grid.leftTriangle()}
+      stroke="#005CFF"
+      fill="#f5f5f5"
+      on:click={getCurrent("n")}
+  />
+  <polygon
+      points={grid.rightTriangle()}
+      stroke="#005CFF"
+      fill="#f5f5f5"
+      on:click={getCurrent("s")}
+  />
+  <g>
+    <text className="timestamp" textAnchor="middle" x="-1.5" y="-0.5">
+    </text>
+  </g>
+</svg>
+<Trains result={result} stations={locations()} />
